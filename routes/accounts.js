d36aaf9cc68ebdac1ea0074cf66ecea65ffa4844
oaklineworkspace <oaklineworkspace@gmail.com>
2025-09-20 13:46:50 -0700

    Update accounts.js

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,10 +1,13 @@
 // routes/accounts.js
 import express from 'express';
+import { verifyToken } from '../middleware/authMiddleware.js';
+
+// Controllers
 import { getAccountTypes } from '../controllers/accounts/accountTypes.js';
 import { createAccount } from '../controllers/accounts/accountCreationController.js';
 import { updateBalance } from '../controllers/accounts/balanceController.js';
 import { transferFunds, getTransactionHistory } from '../controllers/accounts/transferController.js';
-import { verifyToken } from '../middleware/authMiddleware.js';
+import { getUserAccounts } from '../controllers/accounts/accounts.js';
 
 const router = express.Router();
 
@@ -32,5 +35,10 @@ router.post('/transfer', verifyToken, transferFunds);
 // Transaction History
 // =====================
 router.get('/:accountId/transactions', verifyToken, getTransactionHistory);
+
+// =====================
+// Get All Accounts for Logged-in User
+// =====================
+router.get('/user', verifyToken, getUserAccounts);
 
 export default router;
